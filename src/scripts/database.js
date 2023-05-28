475c7daeaf793c895965e4f6e31389828e0fa498
HalianRC <125613474+HalianRC@users.noreply.github.com>
2023-05-28 20:01:09 -0300

    rede-social

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,4 +1,4 @@
-export const users = [
+ const users = [
   {
     id: 1,
     user: "Samuel Leão",
@@ -31,7 +31,7 @@ export const users = [
   },
 ];
 
-export const posts = [
+ const posts = [
   {
     id: 1,
     title:
@@ -63,7 +63,7 @@ export const posts = [
   },
 ];
 
-export const suggestUsers = [
+const suggestUsers = [
   {
     id: 1,
     user: "Carlos Lima",
@@ -83,3 +83,5 @@ export const suggestUsers = [
     img: "./src/assets/img/user7.svg",
   },
 ];
+
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,91 @@
-import { suggestUsers } from './database'
+// import { suggestUsers } from './database'
+
+{/* <article class="article__posts">
+<div class="div__secundary">
+  <img src="./src/assets/img/user2.svg" alt="">
+  <div class="div__secundary-profile">
+    <h3>Samuel Persuhn</h3>
+    <p>Front end Engineer</p>
+  </div>
+</div>
+<div class="secundary__post-user">
+  <h2>Empresa de tecnologia de informção abre vagas para programa de estágio</h2>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, rerum autem? Consequatur non, blanditiis quis ea dolorem in, rem dignissimos magni</p>
+</div>
+<div class="secundary__buttons">
+  <button class="btn__open-post">Abrir Post</button>
+  <div class="btn__like__container">
+    <button class="btn__like"><img src="./src/assets/img/logo.svg" alt=""></button>
+    <p>25</p>
+  </div>
+</div>
+</article> */}
+
+function addCardPaint(posts) {
+  const sectionContainerSecundary = document.querySelector(".section__container-secundary");
+
+  for (let i = 0; i < posts.length; i++) {
+    const articlePosts = document.createElement("article");
+    articlePosts.className = "article__posts";
+
+    const divSecundary = document.createElement("div");
+    divSecundary.className = "div__secundary";
+
+    const imageSecundary = document.createElement("img");
+    imageSecundary.src = posts[i].img;
+    
+    const divSecundaryProfile = document.createElement("div");
+    divSecundaryProfile.className = "div__secundary-profile";
+    
+    const divSecundaryProfile_name = document.createElement("h3");
+    divSecundaryProfile_name.innerText = posts[i].user;
+    // divSecundaryProfile_name.className = "profile__container";
+    
+    const divSecundaryProfile_office = document.createElement("p");
+    divSecundaryProfile_office.innerText = posts[i].stack;
+    // divSecundaryProfile_office.className = "div__suggestion-profile";
+    
+    const secundaryPostUser = document.createElement("div");
+    secundaryPostUser.className = "secundary__post-user";
+    
+    const PostUserTitle = document.createElement("h2");
+    PostUserTitle.textContent = posts[i].title;
+    // PostUserTitle.className = "profile__name";
+    
+    const PostUserText = document.createElement("p");
+    PostUserText.textContent = posts[i].text;
+    // PostUserText.className = "profile__office";
+    
+    const secundaryButtons = document.createElement("div");
+    secundaryButtons.className = "secundary__buttons";
+    
+    const btnOpenPost = document.createElement("button");
+    btnOpenPost.className = "btn__open-post";
+    btnOpenPost.innerText = "Abrir Post"
+
+    const btnLikeContainer = document.createElement("div");
+    btnLikeContainer.className = "btn__like__container";
+
+    const btnLike = document.createElement("button");
+    btnLike.className = "btn__like";
 
 
+    const btnLikeNumber = document.createElement("p");
+    btnLikeNumber.textContent = posts[i].likes;
+    
+    btnLikeContainer.append(btnLike,btnLikeNumber)
+    secundaryButtons.append(btnOpenPost,btnLikeContainer);
+    secundaryPostUser.append(PostUserTitle,PostUserText); 
+    divSecundaryProfile.append(divSecundaryProfile_name,divSecundaryProfile_office);
+    divSecundary.append(imageSecundary,divSecundaryProfile);
+    articlePosts.append(divSecundary,secundaryPostUser,secundaryButtons);
+    sectionContainerSecundary.appendChild(articlePosts); 
+  }
+}
+addCardPaint(posts)
 
-function addCardPaint(suggestUsers) {
-  const sectionSeggestion = document.querySelector(".section__suggestion");
+function followPeople(suggestUsers) {
+  const sectionSeggestion = document.querySelector("#suggestionContent");
 
   for (let i = 0; i < suggestUsers.length; i++) {
     const divSugestion = document.createElement("div");
@@ -34,6 +116,7 @@ function addCardPaint(suggestUsers) {
 
     const btnProfile = document.createElement("button");
     btnProfile.className = "btn__profile";
+    btnProfile.innerText = "Seguir"
 
     divSuggestionProfile.append(profileName, profileOffice);
     profileContainer.append(divSuggestionProfile, btnProfile); 
@@ -44,24 +127,41 @@ function addCardPaint(suggestUsers) {
   }
 }
 
-addCardPaint(suggestUsers)
+followPeople(suggestUsers)
 
-{/* <aside class="aside__container">
-<section class="section__suggestion">          
-  <h2 class="suggestion__title">Sugestões para você seguir</h2>
-   <div class="div__suggestion">
-    <div class="div__container-sugestion">
-      <figure class="suggestion__figure">
-        <img src="./src/assets/img/user2.svg" alt="prfl">
-      </figure>
-      <form class="profile__container">
-        <div class="div__suggestion-profile">
-          <h2 class="profile__name">Carlos Lima</h2>
-          <p class="profile__office">UX e UI Desgner</p>
-        </div>
-        <button class="btn__profile">Seguir</button>
-      </form>
-    </div> 
-  </div>
-</section> 
-</aside> */}
+
+function followButton() {
+  const buttonsFollow = document.querySelectorAll(".btn__profile");
+
+  buttonsFollow.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+      e.preventDefault();
+      button.classList.toggle("btn__profile");
+      button.classList.toggle("btn__profile--actived");
+      button.classList.toggle("no-hover")
+   
+      if (button.classList.contains("btn__profile")) {
+        button.innerText = "Seguir";
+      } else {
+        button.innerText = "Seguindo";
+      }
+    });
+  });
+}
+
+followButton();
+
+
+function likeButton() {
+  const buttonLike = document.querySelectorAll(".btn__like");
+
+  buttonLike.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+      e.preventDefault();
+      button.classList.toggle("btn__like");
+      button.classList.toggle("btn__like--actived");
+    });
+  });
+}
+
+likeButton()
